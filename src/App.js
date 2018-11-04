9df077fcb7b04d75ccfff579cfce67d672228559
VaniushaR <vaniusha.ra@gmail.com>
2018-11-04 11:17:00 -0600

    Agregando componente new task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+//data from .json
+import { ToDo } from './data.json';
+import NewTask from './Components/NewTaskForm';
+//console.log(ToDo);
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      toDo: ToDo
+    };
+    this.handleAddToDo = this.handleAddToDo.bind(this);
+  }
+
+  //function to delete one by one task
+  deleteTask(task) {
+    this.setState({
+      toDo: this.state.toDo.filter((e, i) => {
+        return i !== task;
+      })
+    });
+  }
+  //function sended by props
+  handleAddToDo(task) {
+    this.setState({
+      toDo: [...this.state.toDo, task]
+    });
+  }
   render() {
+    console.log(toDo);
+    const toDo = this.state.toDo.map((task, i) => {
+      return (
+        <div key={i}>
+          <h3>{task.title}</h3>
+          <h4>{task.priority}</h4>
+          <h4>{task.responsible}</h4>
+          <h5>{task.description}</h5>
+          <button onClick={this.deleteTask.bind(this, i)}>Delete</button>
+        </div>
+      );
+    });
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>Today Vania's ToDo List</h1>
+          <span>{this.state.toDo.length}</span>
         </header>
+        <NewTask onAddToDo={this.handleAddToDo} />
+        <div>{toDo}</div>
       </div>
     );
   }
---
+++ src/Components/NewTaskForm.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+
+class NewTask extends Component {
+  constructor() {
+    super();
+    this.state = {
+      title: '',
+      responsible: '',
+      priority: 'low',
+      description: ''
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  //function to submit the new task
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.onAddToDo(this.state);
+    this.setState({
+      title: '',
+      responsible: '',
+      priority: 'low',
+      description: ''
+    });
+  }
+
+  handleInputChange(e) {
+    const { value, name } = e.target;
+    console.log(value, name);
+    this.setState({
+      [name]: value
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="title"
+            value={this.state.title}
+            onChange={this.handleInputChange}
+            placeholder="New Task"
+          />
+          <input
+            type="text"
+            name="responsible"
+            value={this.state.responsible}
+            onChange={this.handleInputChange}
+            placeholder="Responsible"
+          />
+          <input
+            type="text"
+            name="description"
+            value={this.state.description}
+            onChange={this.handleInputChange}
+            placeholder="Description"
+          />
+          <select
+            name="priority"
+            value={this.state.priority}
+            onChange={this.handleInputChange}
+          >
+            <option>low</option>
+            <option>medium</option>
+            <option>high</option>
+          </select>
+          <button type="submit">Save</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default NewTask;
