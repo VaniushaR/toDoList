bb35d643b2782fb5eeee7cf32370df705bf5a695
VaniushaR <vaniusha.ra@gmail.com>
2018-11-04 19:13:50 -0600

    Updates

---
+++ precache-manifest.d32f69e6eacfa805740ecf4281a47ac4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/toDoList/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6603c14d6b98c47b0557",
+    "url": "/toDoList/static/js/runtime~main.6603c14d.js"
+  },
+  {
+    "revision": "ae9fe0d2c29984fd4b6b",
+    "url": "/toDoList/static/js/main.ae9fe0d2.chunk.js"
+  },
+  {
+    "revision": "11441c2014aef5a430d5",
+    "url": "/toDoList/static/js/1.11441c20.chunk.js"
+  },
+  {
+    "revision": "ae9fe0d2c29984fd4b6b",
+    "url": "/toDoList/static/css/main.73a1deda.chunk.css"
+  },
+  {
+    "revision": "eb9b11dda923cf401e801315de79876d",
+    "url": "/toDoList/index.html"
+  }
+];
